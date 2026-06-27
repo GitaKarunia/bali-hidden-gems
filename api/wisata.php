77588f6cf2277535a7e14241eacc40185a4067a6
GitaKarunia <117230051@student.upnyk.ac.id>
2026-06-27 18:16:27 +0700

    Update wisata.php

--- api/wisata.php
+++ api/wisata.php
@@ -1,20 +1,25 @@
 <?php
+header('Content-Type: application/json');
 
-require_once __DIR__ . "/../koneksi.php";
+require_once __DIR__ . "/koneksi.php"; // ← PAKAI PATH YANG BARU NANTI DI CEK YAA GIT -Yudha
 
-$query = pg_query($conn, "
-SELECT *
-FROM wisata
-ORDER BY nama_wisata
-");
+if (!$conn) {
+    http_response_code(500);
+    echo json_encode(["error" => "Koneksi database gagal"]);
+    exit;
+}
+
+$query = pg_query($conn, "SELECT * FROM wisata ORDER BY nama_wisata");
+
+if (!$query) {
+    http_response_code(500);
+    echo json_encode(["error" => pg_last_error($conn)]);
+    exit;
+}
 
 $data = [];
-
-while($row = pg_fetch_assoc($query)){
+while ($row = pg_fetch_assoc($query)) {
     $data[] = $row;
 }
 
-header('Content-Type: application/json');
 echo json_encode($data);
-
-?>
